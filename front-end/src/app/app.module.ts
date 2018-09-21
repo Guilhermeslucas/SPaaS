aef5a97673f22b00c2d7ba029c37b537cd84df8f
guilhermeslucas <guilherme.slucas@gmail.com>
2018-09-21 18:37:43 +0000

    adding the three main angular components

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DefinitionComponent } from './definition/definition.component';
+import { StatusComponent } from './status/status.component';
+import { ResultsComponent } from './results/results.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefinitionComponent,
+    StatusComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule
---
+++ front-end/src/app/definition/definition.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-definition',
+  templateUrl: './definition.component.html',
+  styleUrls: ['./definition.component.css']
+})
+export class DefinitionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
