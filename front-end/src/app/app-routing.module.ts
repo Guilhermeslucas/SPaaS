57b0b5dee554dfa7c35375fb922b3666b8674b0f
guilhermeslucas <guilherme.slucas@gmail.com>
2018-09-29 21:41:03 +0000

    html and css to login page

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { DefinitionComponent } from './definition/definition.component';
 import { StatusComponent} from './status/status.component';
 import { ResultsComponent } from './results/results.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/definition', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'definition', component: DefinitionComponent },
   { path: 'status', component: StatusComponent },
-  { path: 'results', component: ResultsComponent }
+  { path: 'results', component: ResultsComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 
