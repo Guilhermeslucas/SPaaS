15eb05aedb83095094729854de93f60c251de1c7
guilhermeslucas <guilherme.slucas@gmail.com>
2018-10-10 19:40:46 +0000

    adding post button

--- front-end/src/app/data-management/data-management.component.ts
+++ front-end/src/app/data-management/data-management.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-data-management',
@@ -7,11 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataManagementComponent implements OnInit {
   loggedMail: string;
-
+  fileToUpload: File;
   constructor() { }
 
   ngOnInit() {
     this.loggedMail = localStorage.getItem('loggedMail');
+    this.fileToUpload = null;
   }
+
+  onFileChange(files: FileList) {
+    this.fileToUpload = files.item(0);
+    console.log(this.fileToUpload);
+  }
+
+  // postFile(fileToUpload: File): Observable<boolean> {
+  //   const endpoint = 'your-destination-url';
+  //   const formData: FormData = new FormData();
+  //   formData.append('fileKey', fileToUpload, fileToUpload.name);
+  //   return this.httpClient
+  //     .post(endpoint, formData, { headers: yourHeadersConfig })
+  //     .map(() => { return true; })
+  //     .catch((e) => this.handleError(e));
+  // }
+
+
 
 }
