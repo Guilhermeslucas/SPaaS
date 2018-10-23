7cc9986c9b4f6218dead8b150dc149e61367292f
guilhermeslucas <guilherme.slucas@gmail.com>
2018-10-23 17:36:23 +0000

    adding list for tool and data

--- front-end/src/app/tasks-management/tasks-management.component.ts
+++ front-end/src/app/tasks-management/tasks-management.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { SpassService } from '../spass.service';
 
 @Component({
   selector: 'app-tasks-management',
@@ -8,14 +9,24 @@ import { Router } from '@angular/router';
 })
 export class TasksManagementComponent implements OnInit {
   loggedMail: string;
+  dataNames: any;
+  toolNames: any;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private apiService: SpassService) { }
 
   ngOnInit() {
     this.loggedMail = localStorage.getItem('loggedMail');
     if (!this.loggedMail) {
       this.router.navigate(['/', 'login']);
     }
+
+    this.apiService.getBlobFiles().subscribe(response => {
+      this.dataNames = response.replace('[', '').replace(']', '').split('"').join('').replace(' ', '').split(',');
+    });
+
+    this.apiService.getTools().subscribe(response => {
+      this.toolNames = response.replace('[', '').replace(']', '').split('"').join('').replace(/\s/g, '').split(',');
+    });
   }
 
 }
