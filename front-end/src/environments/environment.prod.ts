8dd126f1324add8ef4377c4a8e814cce111f686b
guilhermeslucas <guilherme.slucas@gmail.com>
2018-11-20 01:46:02 +0000

    adding instructions to run

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost:5000',
+  createUserEndpoint: '/api/users/create/',
+  authUserEndpoint: '/api/users/authenticate/',
+  uploadDataEndpoint: '/api/data/upload/',
+  getFilesEndpoint: '/api/data/',
+  uploadToolsEndpoint: '/api/tools/upload/',
+  getToolsEndpoint: '/api/tools/',
+  deleteToolsEnpoint: '/api/tools/',
+  deleteDataEndpoint: '/api/data/',
+  getParametersEndpoint: '/api/tasks/parameters/',
+  submitTaskEndpoint: '/api/tasks/submit/',
+  getResultsBlobEndpoints: '/api/results/'
 };
